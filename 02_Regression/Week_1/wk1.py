90f3efde6147c096f1e7643df60ae998055fe077
Satoshi Nagayasu <snaga@uptime.jp>
2015-12-28 15:51:53 +0900

    Code clean up for Week 1.

--- 02_Regression/Week_1/wk1.py
+++ 02_Regression/Week_1/wk1.py
@@ -1,76 +1,20 @@
 #!/usr/bin/env python2.7
 
+import sys
+sys.path.append("..") 
+
 import graphlab
+
+from regression import simple_linear_regression
+from regression import get_regression_predictions
+from regression import get_residual_sum_of_squares
+from regression import inverse_regression_predictions
 
 sales = graphlab.SFrame('kc_house_data.gl/')
 
 train_data,test_data = sales.random_split(.8,seed=0)
 
 print(train_data.head())
-
-def simple_linear_regression(input_feature, output):
-    intercept = 0.0
-    slope = 0.0
-
-    print("input feature: " + str(input_feature.head()))
-    print("output: " + str(output.head()))
-
-    x = input_feature
-    y = output
-    yx = y * x
-    xx = x * x
-    N = input_feature.size()
-
-    w1 = (yx.sum() - y.sum() * x.sum() / N) / ( xx.sum() - x.sum() * x.sum() / N)
-    print("w1 = %f" % w1)
-
-    w0 = y.mean() - w1 * x.mean()
-    print("w0 = %f" % w0)
-
-    # compute the mean of  input_feature and output
-    # compute the product of the output and the input_feature and its mean
-    # compute the squared value of the input_feature and its mean
-    # use the formula for the slope
-    # use the formula for the intercept
-    
-    intercept = w0
-    slope = w1
-
-    return(intercept, slope)
-
-def get_regression_predictions(input_feature, intercept, slope):
-    x = input_feature
-    w0 = intercept
-    w1 = slope
-
-    y = w0 + w1 * x
-
-    predicted_output = y
-    return(predicted_output)
-
-def get_residual_sum_of_squares(input_feature, output, intercept, slope):
-    y = output
-    x = input_feature
-    w0 = intercept
-    w1 = slope
-
-    pred_y = w0 + x * w1
-    print("Predicted Y: " + str(pred_y.head()))
-    r = y - pred_y
-    print("Redidual: " + str(r.head()))
-    
-    RSS = (r * r).sum()
-    return(RSS)
-
-def inverse_regression_predictions(output, intercept, slope):
-    w0 = intercept
-    w1 = slope
-    y = output
-
-    x = (y - w0) / w1
-
-    estimated_input = x
-    return(estimated_input)
 
 print("*** sqft to price")
 sqft_intercept,sqft_slope = simple_linear_regression(train_data['sqft_living'], train_data['price'])
@@ -104,3 +48,4 @@ print "RSS with sqft: " + str(rss)
 
 rss = get_residual_sum_of_squares(test_data['bedrooms'],test_data['price'],bedroom_intercept,bedroom_slope)
 print "RSS with bedroom: " + str(rss)
+
--- 02_Regression/regression.py
+++ 02_Regression/regression.py
@@ -2,6 +2,80 @@
 
 import graphlab
 import numpy as np # note this allows us to refer to numpy as np instead 
+import unittest
+
+# ---------------------------------------------
+# Week 1: Simple Linear Regression
+# ---------------------------------------------
+def simple_linear_regression(input_feature, output, verbose=False):
+    intercept = 0.0
+    slope = 0.0
+
+    if verbose is True:
+        print("input feature: " + str(input_feature.head()))
+        print("output: " + str(output.head()))
+
+    x = input_feature
+    y = output
+    yx = y * x
+    xx = x * x
+    N = input_feature.size()
+
+    w1 = (yx.sum() - y.sum() * x.sum() / N) / ( xx.sum() - x.sum() * x.sum() / N)
+    if verbose is True:
+        print("w1 = %f" % w1)
+
+    w0 = y.mean() - w1 * x.mean()
+    if verbose is True:
+        print("w0 = %f" % w0)
+
+    # compute the mean of  input_feature and output
+    # compute the product of the output and the input_feature and its mean
+    # compute the squared value of the input_feature and its mean
+    # use the formula for the slope
+    # use the formula for the intercept
+    
+    intercept = w0
+    slope = w1
+
+    return(intercept, slope)
+
+def get_regression_predictions(input_feature, intercept, slope):
+    x = input_feature
+    w0 = intercept
+    w1 = slope
+
+    y = w0 + w1 * x
+
+    predicted_output = y
+    return(predicted_output)
+
+def get_residual_sum_of_squares(input_feature, output, intercept, slope, verbose=False):
+    y = output
+    x = input_feature
+    w0 = intercept
+    w1 = slope
+
+    pred_y = w0 + x * w1
+    if verbose is True:
+        print("Predicted Y: " + str(pred_y.head()))
+    r = y - pred_y
+    if verbose is True:
+        print("Redidual: " + str(r.head()))
+    
+    RSS = (r * r).sum()
+    return(RSS)
+
+def inverse_regression_predictions(output, intercept, slope):
+    w0 = intercept
+    w1 = slope
+    y = output
+
+    x = (y - w0) / w1
+
+    estimated_input = x
+    return(estimated_input)
+
 
 def polynomial_sframe(feature, degree):
     # assume that degree >= 1
@@ -26,7 +100,7 @@ def polynomial_sframe(feature, degree):
 
     return poly_sframe
 
-def get_residual_sum_of_squares(model, data, outcome):
+def get_residual_sum_of_squares_(model, data, outcome):
     RSS = None
     data['prediction'] = model.predict(data)
     data['error'] = outcome - data['prediction']
@@ -62,3 +136,43 @@ def predict_output(feature_matrix, weights):
     # create the predictions vector by using np.dot()
     predictions = np.dot(feature_matrix, weights)
     return(predictions)
+
+class TestWeek1(unittest.TestCase):
+    def test_simple_linear_regression_001(self):
+        sales = graphlab.SFrame('kc_house_data.gl/')
+        train_data,test_data = sales.random_split(.8,seed=0)
+
+        sqft_intercept,sqft_slope = simple_linear_regression(train_data['sqft_living'], train_data['price'])
+
+        self.assertEqual(-47116.07657494047, sqft_intercept)
+        self.assertEqual(281.9588385676974, sqft_slope)
+
+    def test_get_regression_predictions_001(self):
+        sqft_intercept = -47116.07657494047
+        sqft_slope = 281.9588385676974
+
+        est_price = get_regression_predictions(2650, sqft_intercept, sqft_slope)
+
+        self.assertEqual(700074.8456294576, est_price)
+
+    def test_get_residual_sum_of_squares_001(self):
+        sales = graphlab.SFrame('kc_house_data.gl/')
+        train_data,test_data = sales.random_split(.8,seed=0)
+
+        sqft_intercept = -47116.07657494047
+        sqft_slope = 281.9588385676974
+
+        rss = get_residual_sum_of_squares(train_data['sqft_living'], train_data['price'],sqft_intercept,sqft_slope)
+
+        self.assertEqual(1201918356321967.5, rss)
+
+    def test_inverse_regression_predictions_001(self):
+        sqft_intercept = -47116.07657494047
+        sqft_slope = 281.9588385676974
+
+        est_sqft = inverse_regression_predictions(800000,sqft_intercept,sqft_slope)
+
+        self.assertEqual(3004.3962476159463, est_sqft)
+
+if __name__ == '__main__':
+    unittest.main()
